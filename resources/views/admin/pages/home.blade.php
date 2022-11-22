a7692bfc8b26b781895c5f4d987a17758a9a976a
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2022-11-21 23:05:00 -0500

    Corrección de error en el sidebar del home

--- resources/views/admin/pages/home.blade.php
+++ resources/views/admin/pages/home.blade.php
@@ -28,20 +28,20 @@
 @endsection
 
 @section('content-header')
-    @include('admin.partials.content-header', [
-        'title' => 'Dashboard',
-        'items' => [],
-    ])
+@include('admin.partials.content-header', [
+'title' => 'Dashboard',
+'items' => [],
+])
 @endsection
 
 @section('content')
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-         {{--   <h4>Funcionalidad Prevista para Software, Dashboard con diferentes tarjetas de información..</h4> --}}
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        {{-- <h4>Funcionalidad Prevista para Software, Dashboard con diferentes tarjetas de información..</h4> --}}
 
-           <div class="row">
+        <div class="row">
             <div class="col-12">
 
 
@@ -158,63 +158,50 @@
             </div>
             <!-- /.col -->
         </div>
-        </div>
-        <!-- /.container-fluid -->
-    </section>
+    </div>
+    <!-- /.container-fluid -->
+</section>
 
 
-    
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
 
-            <div class="card">
-                <div class="card-header  border-info">
-                    <h3 class="card-title"><strong> <em class="far fa-calendar-alt"></em>
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+
+        <div class="card">
+            <div class="card-header  border-info">
+                <h3 class="card-title"><strong> <em class="far fa-calendar-alt"></em>
                         Calendario</strong> </h3>
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body">
-
-                              <!-- Calendar -->
-                              <div class="card bg-gradient-info">
-                                <div class="card-header border-0">
-                  
-                                  <h3 class="card-title">
-                                  </h3>
-   
-                                  <!-- /. tools -->
-                                </div>
-                                <!-- /.card-header -->
-                                <div class="card-body pt-0">
-                                  <!--The calendar -->
-                                  <div  id="calendar" style="width: 100%"></div>
-                                </div>
-                                <!-- /.card-body -->
-                              </div>
-        
-
-                   
-                </div>
-                <!-- /.card-body -->
-
-
             </div>
+            <!-- /.card-header -->
+            <div class="card-body">
 
-          
-                        
-                
-            
+                <!-- Calendar -->
+                <div class="card bg-gradient-info">
+                    <div class="card-header border-0">
+
+                        <h3 class="card-title">
+                        </h3>
+
+                        <!-- /. tools -->
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body pt-0">
+                        <!--The calendar -->
+                        <div id="calendar" style="width: 100%"></div>
+                    </div>
+                    <!-- /.card-body -->
+                </div>
             </div>
-   
-  
+            <!-- /.card-body -->
+        </div>
+    </div>
 
-        
-        <!-- /.container-fluid -->
-    </section>
+    <!-- /.container-fluid -->
+</section>
 
 
-    
+
 
 
 @endsection
@@ -222,37 +209,37 @@
 
 
 @section('js')
-    <!-- DataTables  & Plugins -->
-    <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
-    <script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
-    <script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<!-- DataTables  & Plugins -->
+<script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
 
-<script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
-    <script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
-    <script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+{{-- <script src="{{ asset('plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ asset('plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
+<script src="{{ asset('plugins/jquery-knob/jquery.knob.min.js') }}"></script>
+--}}
+<!-- Select2 -->
+<script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
 
-        <!-- Select2 -->
-        <script src="{{ asset('plugins/select2/js/select2.full.min.js') }}"></script>
-
-        <!-- daterangepicker -->
-    <script src=" {{ asset('plugins/moment/moment.min.js') }} "></script>
-    <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}  "></script>
+<!-- daterangepicker -->
+<script src=" {{ asset('plugins/moment/moment.min.js') }} "></script>
+<script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}  "></script>
 
 
-    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js') }}"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
@@ -268,20 +255,9 @@
 <!-- daterangepicker -->
 <script src="{{ asset('plugins/moment/moment.min.js') }}"></script>
 <script src="{{ asset('plugins/daterangepicker/daterangepicker.js') }}"></script>
+
 <!-- Tempusdominus Bootstrap 4 -->
 <script src="{{ asset('plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}"></script>
-<!-- Summernote -->
-<script src="{{ asset('plugins/summernote/summernote-bs4.min.js') }}"></script>
-<!-- overlayScrollbars -->
-<script src="{{ asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-<!-- AdminLTE App -->
-<script src="{{ asset('dist/js/adminlte.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('dist/js/demo.js') }}"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="{{ asset('dist/js/pages/dashboard.js') }}"></script>
-
-
 @endsection
 
 
